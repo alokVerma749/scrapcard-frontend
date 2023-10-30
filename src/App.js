77f99d632591218548034621d63df68dd5eaf685
alok verma <alokverma749@gmail.com>
2023-10-30 22:15:30 +0530

    initial setup, routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Registration from './page/Registration';
+import Login from './page/Login';
+import Header from './components/Header';
+import Hero from './page/Hero';
+import Profile from './page/Profile';
+import Footer from './components/Footer';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Hero />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/registration",
+      element: <Registration />
+    },
+    {
+      path: "/profile",
+      element: <Profile />
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gradient-to-br from-blue-500 to-blue-900 text-white min-h-screen">
+      <Header />
+      <RouterProvider router={router}>
+        <Hero />
+      </RouterProvider>
+      <Footer />
     </div>
   );
 }
